793f63099d89f51882f4a34d228fc464977f3a82
Nick Rafferty <nickrafferty78@gmail.com>
2020-11-22 14:47:40 -0600

    Initial Setup for tutorial
    Most of the setup came from https://github.com/puckel/docker-airflow
    
    Initial dag starter code came from airflow tutorial on docs. Just wanted to
    get things working.

---
+++ dags/tutorial.py
@@ -0,0 +1,35 @@
+from datetime import timedelta
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+# Operators; we need this to operate!
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+dag = DAG(
+    'tutorial',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag,
+)
+
+
+t1 
